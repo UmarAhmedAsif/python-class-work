94e0646aaf1a881db4c01e8558259385ce32a079
UmarAhmedAsif <157402084+UmarAhmedAsif@users.noreply.github.com>
2024-02-21 13:27:16 +0500

    Add files via upload

---
+++ class example.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Welcome Bano Qabil")
+st.write("Pythan CIT  by Sir Ghufran Kamaluddin")
+st.title("Campus: PCHMC")
+st.write("Timing 8 to 10")
+st.title ("Final Projects")
+
+
