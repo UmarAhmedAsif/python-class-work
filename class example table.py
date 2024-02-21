360418b6ad5bd3be57f468d49659a214b6ec4b44
UmarAhmedAsif <157402084+UmarAhmedAsif@users.noreply.github.com>
2024-02-21 13:32:57 +0500

    Add files via upload

--- class example table.py
+++ class example table.py
@@ -1,11 +1,14 @@
+
 import streamlit as st
 import pandas as pd
 st.title("Welcome Bano Qabil")
 
 #creating dataset
 
-df=pd.DataFrame({'first':[1,2,3,4,5,],'second':[6,7,8,9,10]})
+df=pd.DataFrame({'first':[1,12,13,54,55,],'second':[66,7,8,9,10]})
 st.write(df)
 
 # or
 df  #magic comments
+st.area_chart(df)
+st.sidebar.bar_chart(df)
