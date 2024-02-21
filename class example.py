e9173ec4a4e44249f3ee4b1533223377f013ac55
UmarAhmedAsif <157402084+UmarAhmedAsif@users.noreply.github.com>
2024-02-21 13:40:32 +0500

    Delete class example.py

--- class example.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("Welcome Bano Qabil")
-st.write("Pythan CIT  by Sir Ghufran Kamaluddin")
-st.title("Campus: PCHMC")
-st.write("Timing 8 to 10")
-st.title ("Final Projects")
-
-
